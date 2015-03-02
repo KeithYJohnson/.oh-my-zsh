772ba4a9bbf87ba1974c0bea1220b883f996b6d2
Keith Johnson <keithjohnson@Keiths-MBP.PK5001Z>
2015-03-02 15:38:50 -0800

    alias to proxy ember server to localhost 3000

--- plugins/ember/ember.plugin.zsh
+++ plugins/ember/ember.plugin.zsh
@@ -1,4 +1,5 @@
 # alias zip='cd ~/projects/zipmark-service'
 
 alias es='ember serve'
+alias es3='ember server --proxy http://localhost:3000'
 alias nombom='npm cache clear && bower cache clean && rm -rf node_modules bower_components && npm install && bower install'
